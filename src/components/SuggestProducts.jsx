5e3d818f2df908149bda7d912c458e94099f905b
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2025-05-21 08:00:54 +0700

    Sửa main flow

--- src/components/SuggestProducts.jsx
+++ src/components/SuggestProducts.jsx
@@ -5,9 +5,9 @@ export const SuggestProducts= ({userId}) =>{
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        //4.1 Kiểm tra xem người dùng đã đăng nhập chưa (userId có null không)
+        //4.1 Kiểm tra xem người dùng đã đăng nhập chưa (userId có null không) và gửi userId về cho server
         const endpoint = userId
-            ? `http://localhost:5000/api/suggested-products/${userId}` //4.2 Gửi userId về server
+            ? `http://localhost:5000/api/suggested-products/${userId}`
             : `http://localhost:5000/api/suggested-products/popular`; //4.6 Người dùng chưa đăng nhập (userId=null)
 
         fetch(endpoint)
