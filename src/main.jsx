f3c98f7aa4d11467c175f4f1284f36a89268f91c
anhtuan2908 <nguyentuan29082004@gmail.com>
2025-05-27 01:42:57 +0700

    them trang confirm checkout

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductDetail from "@/pages/ProductDetail";
 import LoginPage from './pages/LoginPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ConfirmOrderPage from './pages/ConfirmOrderPage';
 
 const RouteSwitch= () => {
     return (
@@ -13,6 +15,8 @@ const RouteSwitch= () => {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/confirm-order" element={<ConfirmOrderPage />} />
             </Routes>
         </BrowserRouter>
     )
